d4caf3f158b3b8bfc0d45d4abef178fe8c981c20
k-dicken <kdicken@iu.edu>
2024-03-26 15:55:09 -0400

    Fixed links added works

---
+++ app/app.js
@@ -0,0 +1,15 @@
+let pieces = [
+    {
+        img: '',
+        size: ''
+    }
+]
+
+//img
+//size (s, m, l)
+//
+
+//function take in class name
+//grab all items work items give them class overlay
+//grab all items with class name and remove class overlay
+
